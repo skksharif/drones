596fe05e6d29395a24ce0b3b60e5a74dd22fe08e
skksharif <khasimsharif12@gmail.com>
2024-11-24 23:03:38 +0530

    Enhnaced

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ function toggleMenu() {
   // Toggle the close icon
   menuIcon.classList.toggle('close');
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingMessage = document.getElementById("loading-message");
+
+  // Simulate a loading time, then hide the message
+  setTimeout(() => {
+      loadingMessage.classList.add("hidden");
+      setTimeout(() => {
+          loadingMessage.style.display = "none";
+      }, 500); // Match the transition duration in CSS
+  }, 3000); // Adjust loading time as needed (3 seconds here)
+});
