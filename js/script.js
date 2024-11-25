175488d4e6cc052d712460a3b9e07e17c0cd9bd0
skksharif <khasimsharif12@gmail.com>
2024-11-25 12:41:45 +0530

    second

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
 function toggleMenu() {
   const navbar = document.querySelector('.navbar');
   const menuIcon = document.querySelector('.menu-icon');
-  
-  // Toggle the menu visibility
   navbar.classList.toggle('show');
-  
-  // Toggle the close icon
   menuIcon.classList.toggle('close');
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const loadingMessage = document.getElementById("loading-message");
 
-  // Simulate a loading time, then hide the message
   setTimeout(() => {
       loadingMessage.classList.add("hidden");
       setTimeout(() => {
           loadingMessage.style.display = "none";
-      }, 500); // Match the transition duration in CSS
-  }, 3000); // Adjust loading time as needed (3 seconds here)
+      }, 500); 
+  }, 3000);
 });
