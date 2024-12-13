14348b8ad0976718abdf29a2d50351c57d3411a2
skksharif <khasimsharif12@gmail.com>
2024-12-13 21:03:43 +0530

    second

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
       }, 500); 
   }, 3000);
 });
+
+document.querySelectorAll('.courses1-read-more').forEach(button => {
+  button.addEventListener('click', () => {
+      const extraContent = button.previousElementSibling;
+      const isHidden = extraContent.style.display === 'none' || !extraContent.style.display;
+      
+      if (isHidden) {
+          extraContent.style.display = 'block';
+          button.textContent = 'Read Less';
+      } else {
+          extraContent.style.display = 'none';
+          button.textContent = 'Read More';
+      }
+  });
+});
+
+document.querySelectorAll('.read-more-button').forEach(button => {
+  button.addEventListener('click', () => {
+      const extraContent = button.previousElementSibling;
+      const isHidden = extraContent.style.display === 'none' || !extraContent.style.display;
+      
+      if (isHidden) {
+          extraContent.style.display = 'block';
+          button.textContent = 'Read Less';
+      } else {
+          extraContent.style.display = 'none';
+          button.textContent = 'Read More';
+      }
+  });
+});
+
